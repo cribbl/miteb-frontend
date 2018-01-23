981e7470df0cd90015a82a63fe99f375f9f3e15c
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-01-24 00:03:51 +0530

    material table ready

--- src/components/ClassroomComponent/MyEventsComponent/MyEventsComponent.js
+++ src/components/ClassroomComponent/MyEventsComponent/MyEventsComponent.js
@@ -1,17 +1,100 @@
 import React, {Component} from 'react';
+import {
+  Table,
+  TableBody,
+  TableFooter,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import Toggle from 'material-ui/Toggle';
+import Paper from 'material-ui/Paper'
+import {tableData} from './data'
+
+import {connect} from 'react-redux'
+
+import SearchSortContainer from './SearchSortContainer'
+
+const styles = {
+  propContainer: {
+    width: 200,
+    overflow: 'hidden',
+    margin: '20px auto 0',
+  },
+  propToggleHeader: {
+    margin: '20px auto 10px',
+  },
+};
 
 class MyEventsComponent extends Component {
-  constructor (props) {
-    super(props)
-  }
+ state = {
+    fixedHeader: true,
+    fixedFooter: false,
+    stripedRows: false,
+    showRowHover: true,
+    showCheckboxes: false,
+  };
 
   render() {
     return (
-    	<div>
-      		<h1> My Events </h1>
-	    </div>
+      <div style={{display: 'flex', justifyContent: 'start', flexDirection: 'column', alignItems: 'center', backgroundColor: '', height: '100%'}}>
+      
+      <div style={{minWidth: '98%', backgroundColor: 'yellow', marginTop: 20}}>
+        <SearchSortContainer />
+      </div>
+      
+      <Paper style={{width: '98%', height: 500, overflow: 'hidden'}} zDepth={2}>
+        <Table
+          style={{backgroundColor: ''}}
+          height={'440px'}
+          fixedHeader={this.state.fixedHeader}
+          fixedFooter={this.state.fixedFooter}
+          selectable={this.state.selectable}
+          multiSelectable={this.state.multiSelectable}
+        >
+          <TableHeader
+            displaySelectAll={this.state.showCheckboxes}
+            adjustForCheckbox={this.state.showCheckboxes}
+            enableSelectAll={this.state.enableSelectAll}
+          >
+            <TableRow style={{backgroundColor: '#EFF0F2'}}>
+              <TableHeaderColumn style={{color: '#000', fontWeight: 700}} >TITLE</TableHeaderColumn>
+              <TableHeaderColumn style={{color: '#000', fontWeight: 700}} >START DATE</TableHeaderColumn>
+              <TableHeaderColumn style={{color: '#000', fontWeight: 700}} >STATUS</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={this.state.showCheckboxes}
+            deselectOnClickaway={this.state.deselectOnClickaway}
+            showRowHover={this.state.showRowHover}
+            stripedRows={this.state.stripedRows}
+          >
+            {tableData.map( (row, index) => (
+              <TableRow key={index}>
+                <TableRowColumn>{index}</TableRowColumn>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.status}</TableRowColumn>
+              </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+        </Paper>
+      </div>
     );
   }
 }
 
-export default MyEventsComponent
+function mapStateToProps(state) {
+  const {openSideNav, isMobile} = state.toggler
+  const {user, verified} = state.authentication
+  return {
+    user,
+    openSideNav,
+    verified,
+    isMobile
+  }
+}
+
+export default connect(mapStateToProps)(MyEventsComponent)
---
+++ src/components/ClassroomComponent/MyEventsComponent/data.js
@@ -0,0 +1,56 @@
+
+export const tableData = [
+  {
+    name: 'John Smith',
+    status: 'Employed',
+  },
+  {
+    name: 'Randal White',
+    status: 'Unemployed',
+  },
+  {
+    name: 'Stephanie Sanders',
+    status: 'Employed',
+  },
+  {
+    name: 'Steve Brown',
+    status: 'Employed',
+  },
+  {
+    name: 'Joyce Whitten',
+    status: 'Employed',
+  },
+  {
+    name: 'Samuel Roberts',
+    status: 'Employed',
+  },
+  {
+    name: 'Adam Moore',
+    status: 'Employed',
+  },
+  {
+    name: 'Samuel Roberts',
+    status: 'Employed',
+  },
+  {
+    name: 'Adam Moore',
+    status: 'Employed',
+  },
+  {
+    name: 'Samuel Roberts',
+    status: 'Employed',
+  },
+  {
+    name: 'Adam Moore',
+    status: 'Employed',
+  },
+  {
+    name: 'Samuel Roberts',
+    status: 'Employed',
+  },
+  {
+    name: 'Adam Moore',
+    status: 'Employed',
+  },
+];
+
