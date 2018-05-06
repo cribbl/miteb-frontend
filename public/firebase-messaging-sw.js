a55967357cb9b3a0045f00547253e24eed04f13e
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-05-06 17:25:05 +0530

    bg worker added

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,3 +6,17 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  var notificationTitle = 'Background Message Title';
+  var notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+// [END background_handler]
