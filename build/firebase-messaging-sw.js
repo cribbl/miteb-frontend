040d9d113afc1fe24883f69b8aa2a48335d77a58
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-05-25 20:19:11 +0530

    notification options

--- build/firebase-messaging-sw.js
+++ build/firebase-messaging-sw.js
@@ -10,11 +10,36 @@ const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  var notificationOptions = {
-    title: payload.notification.title,
-    body:  payload.notification.body,
-    icon:  payload.notification.icon
-  };
+
+  var notificationOptions = payload;
+  // var notificationOptions = {
+  //   title: payload.notification.title,
+  //   body:  payload.notification.body,
+  //   icon:  payload.notification.icon,
+  //   vibrate: [100, 50, 100],
+  //   data: {
+  //     dateOfArrival: Date.now(),
+  //     primaryKey: 1
+  //   }
+  // };
 
   return self.registration.showNotification(payload.notification.title, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if(action === 'close') {
+    notification.close();
+  }
+  if(action === 'open') {
+  	clients.openWindow('https://bookings.cribblservices.com/#/dashboard/myEvents')
+  	notification.close();
+  }
+  else {
+    clients.openWindow('https://bookings.cribblservices.com');
+    notification.close();
+  }
 });
