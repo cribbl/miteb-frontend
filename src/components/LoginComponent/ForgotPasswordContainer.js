18662b67d9af0b0a0595bdc6c21149097e115ee2
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-05-29 09:55:07 +0530

    forgot password email, auto redirect to signin

--- src/components/LoginComponent/ForgotPasswordContainer.js
+++ src/components/LoginComponent/ForgotPasswordContainer.js
@@ -4,8 +4,8 @@ import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton'
 import CircularProgress from 'material-ui/CircularProgress'
-
-import { Link } from 'react-router'
+import {sendPasswordResetEmail} from '../../Services/firebaseAuthService'
+import {Link, hashHistory} from 'react-router'
 
 import './LoginComponent.css'
 
@@ -17,7 +17,10 @@ class ForgotPasswordContainer extends Component {
     this.handleForgotPasswordSubmit = this.handleForgotPasswordSubmit.bind(this)
 
     this.state = {
-      username: ''
+      username: '',
+      errorMessage: null,
+      sendingResetEmail: false,
+      success: false
     }
   }
 
@@ -26,7 +29,22 @@ class ForgotPasswordContainer extends Component {
   }
 
 	handleForgotPasswordSubmit (e) {
-
+    e.preventDefault();
+    var scope = this;
+    this.setState({sendingResetEmail: true})
+    sendPasswordResetEmail(this.state.username, (err) => {
+      this.setState({sendingResetEmail: false})
+      if(err) {
+        scope.setState({errorMessage: err.message})
+      }
+      else {
+        scope.setState({success: true})
+        scope.setState({errorMessage: 'A password reset link has been sent to your email!'})
+        setTimeout(function() {
+          hashHistory.push('/auth')
+        }, 5000)
+      }
+    })
   }
 
 	render() {
@@ -36,11 +54,15 @@ class ForgotPasswordContainer extends Component {
         <h2 className="paperTitle">Forgot Password</h2>
         <h6 className="paperDesc">No worries! We'll help you retrieve your account</h6>
           <div className="fieldsContainer">
-            <TextField hintText="Email" value={this.state.username} onChange={this.changeUsername} required />
-            <RaisedButton className="submitButton" type="submit" label="Submit" primary={true} disabled={this.props.logging} />
-            <CircularProgress style={{position: 'absolute', padding: '27px 5px'}} size={20} hidden={!this.props.logging}/>
+            <TextField
+              hintText="Email"
+              value={this.state.username}
+              onChange={this.changeUsername}
+              required />
+            <RaisedButton className="submitButton" type="submit" label="Submit" primary={true} disabled={this.state.sendingResetEmail} />
+            <CircularProgress style={{position: 'absolute', padding: '27px 5px'}} size={20} hidden={!this.state.sendingResetEmail}/>
           </div>
-              {this.props.error && <p>{this.props.error.message}</p>}
+              {<p style={{color: this.state.success ? '#00bcd4' : '#D8000C'}}>{this.state.errorMessage}</p>}
           <br /><br /><br /><br /><br /><br /><Link className="bottomAlign" to="/auth/signin">Back</Link>
       </form>
       </div>
