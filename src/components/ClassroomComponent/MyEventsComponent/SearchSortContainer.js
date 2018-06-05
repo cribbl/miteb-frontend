9de4f2ad8dec2cf7bffae8f9909038b9ab9d6583
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-06-05 19:09:09 +0530

    search bar working

--- src/components/ClassroomComponent/MyEventsComponent/SearchSortContainer.js
+++ src/components/ClassroomComponent/MyEventsComponent/SearchSortContainer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './MyEventsComponent';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import {hashHistory} from 'react-router'
 import {connect} from 'react-redux'
@@ -8,16 +9,23 @@ class SearchSortContainer extends Component {
   constructor (props) {
     super(props)
     this.filterClicked = this.filterClicked.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
     console.log('IPHONE')
     console.log(props)
     this.state = {
-      filter: 'all'
+      filter: 'all',
+      search: ''
     }
   }
 
   filterClicked(filter) {
     const {dispatch} = this.props
     dispatch({type: 'FILTER', filter})
+  }
+
+  handleSearch(e) {
+    this.setState({search: e.target.value})
+    this.props.handleSearch(e.target.value)
   }
 
   render() {
@@ -33,7 +41,10 @@ class SearchSortContainer extends Component {
         </ToolbarGroup>
         {!this.props.isMobile ? 
           <ToolbarGroup>
-            <TextField hintText={this.props.filter}/>
+            <TextField
+              floatingLabelText="Search"
+              value={this.state.search}
+              onChange={this.handleSearch} />
           </ToolbarGroup>
           : '' }
        </Toolbar>
