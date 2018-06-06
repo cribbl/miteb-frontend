97e4a2cbcdbc165a16e01dc43a225820a11f08f7
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-06-06 18:04:13 +0530

    SC View Complaints

--- src/components/ClassroomComponent/ViewComplaintsComponent/ViewComplaintsComponent.js
+++ src/components/ClassroomComponent/ViewComplaintsComponent/ViewComplaintsComponent.js
@@ -13,22 +13,28 @@ import TextField from 'material-ui/TextField';
 import Toggle from 'material-ui/Toggle';
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
+import IconButton from 'material-ui/IconButton'
 import {hashHistory} from 'react-router'
 import {connect} from 'react-redux'
 import {firebaseDB} from '../../../firebaseConfig'
 import Snackbar from 'material-ui/Snackbar';
 import {approveEvent, flagRejectEvent} from '../../../Services/firebaseDBService'
 import Dialogxx from '../../Dialogs/ViewComplaintDialogComponent'
-
+import SortIcon from 'material-ui/svg-icons/content/sort' 
+import UpArrow from 'material-ui/svg-icons/navigation/arrow-upward'
+import DownArrow from 'material-ui/svg-icons/navigation/arrow-downward'
+import moment from 'moment'
+import {toggleActions} from '../../../actions/toggleActions'
 
 class ViewComplaintsComponent extends Component {
   constructor(props) {
     super(props)
     this.showDialog = this.showDialog.bind(this)
     this.handleDialogClose = this.handleDialogClose.bind(this)
-    this.handleSnackBarClose = this.handleSnackBarClose.bind(this)
     this.handleFlagDialogClose = this.handleFlagDialogClose.bind(this)
     this.nextComplaint = this.nextComplaint.bind(this)
+    this.handleSort = this.handleSort.bind(this);
+    this.resolveComplaint = this.resolveComplaint.bind(this)
     
     this.state = {
       fixedHeader: true,
@@ -39,14 +45,11 @@ class ViewComplaintsComponent extends Component {
       myArr: {},
       myArrx: {},
       allArr: {},
-      pendingArr: {},
-      approvedArr: {},
-      SnackBarmessage: '',
-      openSnackBar: false,
-      autoHideDuration: 3000,
+      originalArr: {},
       dialogOpen: false,
       FlagDialogOpen: false,
-      currentComplaint: null
+      currentComplaint: null,
+      dateSort: null,
     }
 }
 
@@ -59,8 +62,14 @@ class ViewComplaintsComponent extends Component {
     this.setState({dialogOpen: false})
   }
 
-  handleSnackBarClose() {
-    this.setState({openSnackBar: false}) 
+  resolveComplaint(complaint, mode) {
+    firebaseDB.ref('complaints/' + complaint.key + '/isResolved').set(true);
+    const {myArrx} = this.state
+    delete myArrx[complaint.key]
+    this.setState({myArrx})
+    this.nextComplaint();
+    const {dispatch} = this.props
+    dispatch(toggleActions.toggleToaster("Complaint Resolved", true))
   }
 
   handleFlagDialogClose() { this.setState({FlagDialogOpen: false}) }
@@ -80,6 +89,23 @@ class ViewComplaintsComponent extends Component {
     this.setState({currentComplaint: nextComplaint})
   }
 
+  handleSort() {
+    if(this.state.dateSort === 'des')
+      this.setState({dateSort: 'asc'})
+    else
+      this.setState({dateSort: 'des'})
+    var scope = this
+    var myArrx = this.state.originalArr
+    myArrx = Object.values(myArrx).sort(function(a, b)
+      { 
+        var aDate = moment(a.start_date, 'DD-MM-YYYY');
+        var bDate = moment(b.start_date, 'DD-MM-YYYY');
+        if(scope.state.dateSort === 'des')
+          return (aDate - bDate);
+        return (bDate - aDate);
+      });
+    this.setState({myArrx})
+  }
 
   componentDidMount() {
     if(!this.props.user){
@@ -93,13 +119,14 @@ class ViewComplaintsComponent extends Component {
           scope.setState({fetching: false})
           snapshot.forEach(function(child) {
             scope.setState({fetching: false})
-                const {myArrx} = scope.state
-                myArrx[child.key] = child.val()
-                myArrx[child.key].key = child.key
-                scope.setState({myArrx})
-                console.log(scope.state.myArrx)
+              const {myArrx} = scope.state
+              myArrx[child.key] = child.val()
+              myArrx[child.key].key = child.key
+              scope.setState({myArrx})
+              scope.setState({originalArr: myArrx})
+              console.log(scope.state.myArrx)
           })
-        }, this)
+        })
   }
 
   render() {
@@ -112,14 +139,7 @@ class ViewComplaintsComponent extends Component {
       </div>*/}
 
       {this.state.currentComplaint && 
-      <Dialogxx open={this.state.dialogOpen} currentComplaint={this.state.currentComplaint} handleClose={this.handleDialogClose} nextComplaint={this.nextComplaint} />}
-
-      <Snackbar
-          open={this.state.openSnackBar}
-          message={this.state.SnackBarmessage}
-          autoHideDuration={this.state.autoHideDuration}
-          onRequestClose={this.handleSnackBarClose}
-        />
+      <Dialogxx open={this.state.dialogOpen} currentComplaint={this.state.currentComplaint} handleClose={this.handleDialogClose} nextComplaint={this.nextComplaint} resolveComplaint={this.resolveComplaint} />}
 
       <Paper style={{width: '98%', height: 500, overflow: 'hidden'}} zDepth={2}>
         <Table
@@ -136,8 +156,14 @@ class ViewComplaintsComponent extends Component {
             enableSelectAll={this.state.enableSelectAll}
           >
             <TableRow style={{backgroundColor: '#EFF0F2'}}>
-              <TableHeaderColumn style={{color: '#000', fontWeight: 700, width: '15%'}}>Address To</TableHeaderColumn>
-              <TableHeaderColumn style={{color: '#000', fontWeight: 700, width: '55%'}}>Complaint</TableHeaderColumn>
+              <TableHeaderColumn style={{color: '#000', fontWeight: 700, width: '15%'}}>Subject</TableHeaderColumn>
+              <TableHeaderColumn
+                style={{color: '#000', fontWeight: 700, display: 'flex', alignItems: 'center', width: '15%'}}
+                hidden={this.props.isMobile}>
+                Dated
+                <IconButton onClick={this.handleSort} style={{padding: 0, height: 20, width: 20}}>{this.state.dateSort!=null ? (this.state.dateSort === 'asc' ? <UpArrow viewBox='0 0 30 30' /> : <DownArrow viewBox='0 0 30 30' />) : <SortIcon viewBox='0 0 30 30' />}</IconButton>
+              </TableHeaderColumn>
+              <TableHeaderColumn style={{color: '#000', fontWeight: 700, width: '35%'}}>Description</TableHeaderColumn>
               <TableHeaderColumn style={{color: '#000', fontWeight: 700, width: '20%'}}>Actions</TableHeaderColumn>
             </TableRow>
           </TableHeader>
@@ -153,8 +179,9 @@ class ViewComplaintsComponent extends Component {
           { Object.keys(this.state.myArrx).length > 0 ? (Object.values(this.state.myArrx).map(function(complaint, index) {
               return (
                   <TableRow key={index}>
-                    <TableRowColumn style={{width: '15%'}}>{complaint.address}</TableRowColumn>
-                    <TableRowColumn style={{width: '55%'}}>{complaint.desc}</TableRowColumn>
+                    <TableRowColumn style={{width: '15%'}}>{complaint.subject}</TableRowColumn>
+                    <TableRowColumn style={{width: '15%'}}>{complaint.dated}</TableRowColumn>
+                    <TableRowColumn style={{width: '35%'}}>{complaint.desc}</TableRowColumn>
                     <TableRowColumn style={{width: '20%'}}>{<div><RaisedButton label="View" primary={true} style={{marginRight: 10}} onClick={() => this.showDialog(complaint)}/><RaisedButton hidden={this.props.isMobile} label="Approve" primary={true} onClick={() => this.approve(complaint)}/></div>}</TableRowColumn>
                   </TableRow>
             )}, this)) : <p style={{textAlign: 'center', fontSize: '3rem'}}>NO EVENTS PENDING</p>
--- src/components/Dialogs/ViewComplaintDialogComponent.js
+++ src/components/Dialogs/ViewComplaintDialogComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton'
+import ReplyIcon from 'material-ui/svg-icons/content/reply'
 import {connect} from 'react-redux'
 
 const styles = {
@@ -21,10 +23,15 @@ const styles = {
 
 class Dialogxx extends Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.sendGmail = this.sendGmail.bind(this)
     this.state = {
       open: this.props.open,
     };
+  }
+
+  sendGmail() {
+    window.open('https://mail.google.com/mail/?view=cm&fs=1&to=' + this.props.currentComplaint.fields.email)
   }
 
   componentWillReceiveProps(nextProps) {
@@ -34,14 +41,14 @@ class Dialogxx extends Component {
   render() {
     const FA_actions = [
       <FlatButton
-        label="Cancel"
+        label="Close"
         primary={false}
         onClick={this.props.handleClose}
       />,
       <FlatButton
-        label="Reject"
+        label="Mars as Resolved"
         primary={false}
-        onClick={() => this.props.flagRejectHandler(this.props.currentComplaint, 'reject')}
+        onClick={() => this.props.resolveComplaint(this.props.currentComplaint, 'resolved')}
       />,
       <FlatButton
         label="Flag"
@@ -62,9 +69,14 @@ class Dialogxx extends Component {
 
     const Club_actions = [
       <FlatButton
-        label="Cancel"
+        label="Close"
         primary={false}
         onClick={this.props.handleClose}
+      />,
+      <FlatButton
+        label="Mark as Resolved"
+        primary={true}
+        onClick={() => this.props.resolveComplaint(this.props.currentComplaint, 'resolved')}
       />,
       <FlatButton
         label="Next"
@@ -93,7 +105,9 @@ class Dialogxx extends Component {
           </div>
           <div style={{border: '1px solid black', display: 'flex', alignItems: 'center'}}>
             <p style={styles.label}>Email</p>
-            <p style={styles.value}>{this.props.currentComplaint.fields.email}</p>
+            <p style={styles.value}>{this.props.currentComplaint.fields.email}
+            <IconButton onClick={this.sendGmail} style={{padding: 0, height: 20, width: 20}}><ReplyIcon viewBox='0 0 30 30' />}</IconButton>
+            </p>
           </div>
           <div style={{border: '1px solid black', display: 'flex', alignItems: 'center'}}>
             <p style={styles.label}>Reg No</p>
@@ -109,6 +123,10 @@ class Dialogxx extends Component {
           </div>
           </div>
         )}
+          <div style={{border: '1px solid black', display: 'flex', alignItems: 'center'}}>
+            <p style={styles.label}>Subject</p>
+            <p style={styles.value}>{this.props.currentComplaint.subject}</p>
+          </div>
           <div style={{border: '1px solid black', display: 'flex', alignItems: 'center'}}>
             <p style={styles.label}>Description</p>
             <p style={styles.value}>{this.props.currentComplaint.desc}</p>
