8a2a278335e87dd8f659e88c5b30ebd7c640a719
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-06-18 13:17:37 +0530

    menuItem active logic changed

--- src/components/DrawerComponent/DrawerComponent.js
+++ src/components/DrawerComponent/DrawerComponent.js
@@ -50,8 +50,12 @@ class DrawerComponent extends Component {
     this.closeSideNav = this.closeSideNav.bind(this);
 
     this.state = {
-      menuIndex: 0
+      activeItem: ''
     }
+
+  hashHistory.listen(location => {
+    this.setState({activeItem: location.pathname})
+  });
   }
 
   closeSideNav () {
@@ -66,9 +70,6 @@ class DrawerComponent extends Component {
       dispatch(toggleActions.closeSideNav())
     }   
     hashHistory.push(menuItem.key);
-
-    this.setState({menuIndex: index})
-
   }
 
   render() {
@@ -80,25 +81,25 @@ class DrawerComponent extends Component {
           <Menu style={menuStyle} onItemClick={this.menuItemClicked}>
 
             <MenuItem
-                style={Object.assign(this.state.menuIndex == 0? active: '', menuItemStyle)}
+                style={Object.assign(this.state.activeItem === '/dashboard' ? active: '', menuItemStyle)}
                 key="/dashboard"
                 primaryText="Dashboard"
                 leftIcon={<IconDashboard color={'#FFFFFF'} />} />
 
             <MenuItem
-                style={Object.assign(this.state.menuIndex == 1 && window.location.hash !== "#/dashboard/myEvents" ? active:'', menuItemStyle)}
+                style={Object.assign(this.state.activeItem === '/dashboard/book_room' ? active:'', menuItemStyle)}
                 key="/dashboard/book_room"
                 primaryText="Room Booking"
                 leftIcon={<IconTest color={'#FFFFFF'} />}
                 hidden={this.props.user && !this.props.user.isClub} />
 
             <MenuItem
-                style={Object.assign((this.state.menuIndex == 2)||(window.location.hash === "#/dashboard/myEvents") ? active :'', menuItemStyle)}
+                style={Object.assign(this.state.activeItem === '/dashboard/myEvents' ? active:'', menuItemStyle)}
                 key="/dashboard/myEvents"
                 primaryText="My Events"
                 leftIcon={<IconAnalytics color={'#FFFFFF'} />} />
             <MenuItem
-                style={Object.assign((this.state.menuIndex == 3) ? active:'', menuItemStyle)}
+                style={Object.assign(this.state.activeItem === '/dashboard/profile' ? active:'', menuItemStyle)}
                 key="/dashboard/profile"
                 primaryText="Profile"
                 leftIcon={<IconProfile color={'#FFFFFF'} />} />
