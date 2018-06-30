e441747dd7dd5b2f7036ec34afbb9c9fe7ad82b9
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-06-30 12:18:13 +0530

    dashboard image

--- src/components/ClassroomComponent/DashboardComponent/DashboardComponent.js
+++ src/components/ClassroomComponent/DashboardComponent/DashboardComponent.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'
+import { Link, hashHistory } from 'react-router'
 
 class DashboardComponent extends Component {
   constructor (props) {
-    super(props)
+    super(props);
   }
 
   render() {
     return (
-    	<div>
-      		<h1> Dashboard </h1>
-	    </div>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: 10}}>
+        <img src={require('../../../assets/analytics.png')} style={{width: this.props.isMobile ? '96%' : '70%'}}/>
+      </div>
     );
   }
 }
 
-export default DashboardComponent
+function mapStateToProps(state) {
+  const {isMobile} = state.toggler
+  return {
+    isMobile
+  }
+}
+
+export default connect(mapStateToProps)(DashboardComponent)
