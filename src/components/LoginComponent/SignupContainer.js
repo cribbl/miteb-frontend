8f2c5b4222eda06bacf331da7c11bb4facad5b23
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-08-19 15:19:22 +0530

    env, fa signup

--- src/components/LoginComponent/SignupContainer.js
+++ src/components/LoginComponent/SignupContainer.js
@@ -4,8 +4,9 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton'
 import CircularProgress from 'material-ui/CircularProgress'
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
-import {createUserWithEmailAndPassword} from '../../Services/firebaseAuthService'
+import {createClubWithEmailAndPassword} from '../../Services/firebaseAuthService'
 
 import { Link, hashHistory } from 'react-router'
 
@@ -25,7 +26,7 @@ class SignupContainer extends Component {
         email : '',
         primaryContact:'',
         password : '',
-        category: '',
+        category: 'technical',
       },
       fieldTouch: {
         name: '',
@@ -37,11 +38,16 @@ class SignupContainer extends Component {
       showProgress: false,
       errorMessage: null,
       signupSuccess: false,
-      counter: 10,
+      counter: 15,
       errors: [],
-      isValid: false
+      isValid: false,
+      tabIndex: 0,
     }
   }
+
+  handleTabChange = (value) => {
+    this.setState({ tabIndex: value });
+  };
 
   handleCounter() {
     var scope = this;
@@ -76,22 +82,25 @@ class SignupContainer extends Component {
     let fieldTouch = this.state.fieldTouch[field]
     let errorText=""
      switch(field) {
-      case 'name':           errorText = value.length<1? "Cannot be empty" : ""
-                             errors[2] = errorText
-                             break;
-      case 'primaryContact': errorText = value.length>=1? !/^[0-9]{10}$/.test(value)? "Please enter a valid phone number" : ""  : "Cannot be empty"
-                             errors[0] = errorText
-                             break;
-      case 'email':          errorText = value.length >= 1?  (!/^(([^[<>()\[\]\\.,;:@"]+(\.[^<>()\[\]\\.,;:@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\.-0-9])+[a-zA-Z]))$/.test(value))? "Email is not valid" : "" : "Cannot be empty"
-                             errors[1] = errorText
-                             break;
-      case 'abbrv':          errorText = value.length<1? "Cannot be empty" : ""
-                             errors[3] = errorText
-                             break;
+      case 'name':
+        errorText = value.length<1? "Cannot be empty" : ""
+        errors[2] = errorText
+        break;
+      case 'primaryContact': 
+        errorText = value.length>=1? !/^[0-9]{10}$/.test(value)? "Please enter a valid phone number" : ""  : "Cannot be empty"
+        errors[0] = errorText
+        break;
+      case 'email':
+        errorText = value.length >= 1?  (!/^(([^[<>()\[\]\\.,;:@"]+(\.[^<>()\[\]\\.,;:@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\.-0-9])+[a-zA-Z]))$/.test(value))? "Email is not valid" : "" : "Cannot be empty"
+        errors[1] = errorText
+        break;
+      case 'abbrv':          
+        errorText = value.length<1? "Cannot be empty" : ""
+        errors[3] = errorText
+        break;
       default: console.log('invalid case')
     }
 
-    //Using the filter method
     let isValid=false;
     var newArray = errors.filter(function(element) {
       if(element != '') return element;
@@ -109,16 +118,32 @@ class SignupContainer extends Component {
     e.preventDefault();
     var newUser = this.state.newUser;
     this.setState({showProgress: true})
-    createUserWithEmailAndPassword(this.state.newUser, (err, res) => {
-      this.setState({showProgress: false})
-      if(err) {
-        this.setState({errorMessage: err.message})
-      }
-      else {
-        this.setState({signupSuccess: true})
-        this.handleCounter();
-      }
-    }, this)
+    console.log(this.state.newUser);
+    if(this.state.tabIndex == 0) {
+      createClubWithEmailAndPassword(this.state.newUser, (err, res) => {
+        this.setState({showProgress: false})
+        if(err) {
+          this.setState({errorMessage: err.message})
+        }
+        else {
+          this.setState({signupSuccess: true})
+          this.handleCounter();
+        }
+      }, this)
+    }
+
+    else {
+      createFAWithEmailAndPassword(this.state.newUser, (err, res) => {
+        this.setState({showProgress: false})
+        if(err) {
+          this.setState({errorMessage: err.message})
+        }
+        else {
+          this.setState({signupSuccess: true})
+          this.handleCounter();
+        }
+      }, this)
+    }
   }
 
   render () {
@@ -141,7 +166,9 @@ class SignupContainer extends Component {
             }
 
           <div className="fieldsContainer" hidden={this.state.signupSuccess}>
+          <Tabs onChange={this.handleTabChange} value={this.state.tabIndex}>
 
+          <Tab label="Club" value={0}>
               <TextField
                 floatingLabelText="Club Name"
                 value={this.state.newUser.name}
@@ -158,6 +185,74 @@ class SignupContainer extends Component {
                 onChange={(event) => this.handleChange(event, 'abbrv')}
                 onBlur={() => this.handleBlur('abbrv')}
                 errorText= {this.state.fieldTouch['abbrv'] && this.state.errors[3]}
+                errorStyle={{position: 'absolute', bottom: -8}}
+                style={{marginTop: -15,marginBottom:8}}
+                required />
+
+              <TextField
+                floatingLabelText="Email"
+                value={this.state.newUser.email}
+                onChange={(event) => this.handleChange(event, 'email')}
+                onBlur={this.handleBlur.bind(this,"email")}
+                errorText={this.state.fieldTouch['email'] && this.state.errors[1]}
+                errorStyle={{position: 'absolute', bottom: -8}}
+                style={{marginTop: -15, marginBottom: 8}}
+                required />
+
+              <TextField
+                floatingLabelText="Primary Phone"
+                type="number"
+                value={this.state.newUser.primaryContact}
+                onChange={(event) => this.handleChange(event, 'primaryContact')}
+                onBlur={() => this.handleBlur('primaryContact')}
+                errorText={this.state.fieldTouch['primaryContact'] && this.state.errors[0]}
+                errorStyle={{position: 'absolute', bottom: -8}}
+                style={{marginTop: -15, marginBottom: 8}}
+                required />
+
+              <TextField
+                floatingLabelText="Password"
+                type="password"
+                value={this.state.newUser.password}
+                onChange={(event) => this.handleChange(event, 'password')}
+                onBlur={() => this.handleBlur('password')}
+                errorText= {this.state.newUser.password_error}
+                errorStyle={{position: 'absolute', bottom: -8}}
+                style={{marginTop: -15,marginBottom:8}}
+                required />
+
+              <RadioButtonGroup name="category" defaultSelected="technical" style={{display: 'inline-flex',marginLeft: -2, width: '105%'}} onChange={(event) => this.handleChange(event, 'category')}>
+                <RadioButton
+                  value="technical"
+                  label="Technical"
+                  style={{width: '37%', backgroundColor: '', marginRight: 10}}
+                  iconStyle={{marginRight: 5}}
+                />
+                <RadioButton
+                  value="nonTechnical"
+                  label="Non Technical"
+                  style={{width: '66%', backgroundColor: ''}}
+                  iconStyle={{marginRight: 5}}
+                />
+              </RadioButtonGroup>
+            </Tab>
+
+            <Tab label="FA" value={1}>
+              <TextField
+                floatingLabelText="Club ID"
+                value={this.state.newUser.name}
+                onChange={(event) => this.handleChange(event, 'name')}
+                onBlur={() => this.handleBlur('name')}
+                errorText= {this.state.fieldTouch['name'] && this.state.errors[2]}
+                errorStyle={{position: 'absolute', bottom: -8}}
+                style={{marginTop: -15,marginBottom:8}}
+                required />
+              <TextField
+                floatingLabelText="Full Name"
+                value={this.state.newUser.name}
+                onChange={(event) => this.handleChange(event, 'name')}
+                onBlur={() => this.handleBlur('name')}
+                errorText= {this.state.fieldTouch['name'] && this.state.errors[2]}
                 errorStyle={{position: 'absolute', bottom: -8}}
                 style={{marginTop: -15,marginBottom:8}}
                 required />
@@ -194,19 +289,8 @@ class SignupContainer extends Component {
                 style={{marginTop: -15,marginBottom:8}}
                 required />
 
-              <RadioButtonGroup name="category" defaultSelected="technical" style={{display: 'inline-flex', marginLeft: -2, width: '105%'}} onChange={(event) => this.handleChange(event, 'category')}>
-                <RadioButton
-                  value="technical"
-                  label="Technical"
-                  style={{width: '40%', backgroundColor: ''}}
-                />
-                <RadioButton
-                  value="nonTechnical"
-                  label="Non Technical"
-                  style={{width: '60%', backgroundColor: ''}}
-                />
-              </RadioButtonGroup>
-
+            </Tab>
+            </Tabs>
                 <RaisedButton className="submitButton" type="submit" label="Sign Up" primary={true} disabled={this.state.showProgress || !this.state.isValid}/>
                 <CircularProgress style={{position: 'absolute', padding: '27px 5px'}} size={20} hidden={!this.state.showProgress}/>
               {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
