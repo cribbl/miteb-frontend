a55c3212b147f56dd31bb34ac7cb49fd6abf42f8
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-08-19 08:58:11 +0530

    ci-cd

---
+++ prod-deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+# Initialize a new git repo in _site, and push it to our server.
+ls
+mv deploy-keys build
+cd build
+git init
+chmod 600 deploy-keys
+
+eval `ssh-agent -s`
+ssh-add deploy-keys
+mkdir .ssh
+touch ~/.ssh/known_hosts
+ssh-keyscan -H 139.59.40.206 >> ~/.ssh/known_hosts
+
+git remote add deploy "deploy@139.59.40.206:/var/www/prod"
+git config user.name "bhawesh96"
+git config user.email "bhansalibhawesh@yahoo.com"
+
+git add .
+git commit -m "Deploy"
+git push --force deploy master
+rm deploy-keys
