f9e3c5e9edeb4799a29ec826a3faa34a3fbf7216
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-18 10:13:22 +0530

    style

--- src/Services/NotificationService.js
+++ src/Services/NotificationService.js
@@ -4,20 +4,25 @@ import { store } from '../store'
 import { toggleActions } from '../actions/toggleActions'
 import { updateToken } from './firebaseDBService'
 
-if (window.location.host.indexOf('prod') > -1) { var base_url = 'https://app-miteventbooking.herokuapp.com' } else { var base_url = 'https://dev-miteventbooking.herokuapp.com' }
+let baseUrl
+if (window.location.host.indexOf('prod') > -1) {
+  baseUrl = 'https://app-miteventbooking.herokuapp.com'
+} else {
+  baseUrl = 'https://dev-miteventbooking.herokuapp.com'
+}
 
-export const sendEmail = (senderName, senderEmail, to, default_purpose, subject = null, text = null, html = null) => {
+export const sendEmail = (senderName, senderEmail, to, defaultPurpose, subject = null, text = null, html = null) => {
   let params = {
     senderName: senderName,
     senderEmail: senderEmail,
     to: to,
-    default_purpose: default_purpose,
+    default_purpose: defaultPurpose,
     subject: subject,
     text: text,
     html: html
   }
 
-  axios.post(base_url + '/notif/send-email', params)
+  axios.post(baseUrl + '/notif/send-email', params)
     .then(function (resp) {
       console.log(resp)
     })
@@ -26,20 +31,20 @@ export const sendEmail = (senderName, senderEmail, to, default_purpose, subject 
     })
 }
 
-export const sendEmailTemplate = (authority = null, mode, message, club_name, club_email, booker_name, booker_email, event_name, receipt_url = null) => {
+export const sendEmailTemplate = (authority = null, mode, message, clubName, clubEmail, bookerName, bookerEmail, eventName, receipt_url = null) => {
   let params = {
     authority: authority,
     mode: mode,
     message: message,
-    club_name: club_name,
-    club_email: club_email,
-    booker_name: booker_name,
-    booker_email: booker_email,
-    event_name: event_name,
+    club_name: clubName,
+    club_email: clubEmail,
+    booker_name: bookerName,
+    booker_email: bookerEmail,
+    event_name: eventName,
     receipt_url: receipt_url
   }
 
-  axios.get(base_url + '/notif/send-email-template', { params })
+  axios.get(baseUrl + '/notif/send-email-template', { params })
     .then(function (resp) {
       console.log(resp)
     })
@@ -48,14 +53,14 @@ export const sendEmailTemplate = (authority = null, mode, message, club_name, cl
     })
 }
 
-export const sendComplaintTemplate = (booker_email, booker_name, subject) => {
+export const sendComplaintTemplate = (bookerEmail, bookerName, subject) => {
   let params = {
-    booker_email: booker_email,
-    booker_name: booker_name,
+    booker_email: bookerEmail,
+    booker_name: bookerName,
     subject: subject
   }
 
-  axios.get(base_url + '/notif/send-complaint-email', { params })
+  axios.get(baseUrl + '/notif/send-complaint-email', { params })
     .then(function (resp) {
       console.log(resp)
     })
@@ -64,13 +69,13 @@ export const sendComplaintTemplate = (booker_email, booker_name, subject) => {
     })
 }
 
-export const sendApproveClubTemplate = (club_email, club_name) => {
+export const sendApproveClubTemplate = (clubEmail, clubName) => {
   let params = {
-    club_email: club_email,
-    club_name: club_name
+    club_email: clubEmail,
+    club_name: clubName
   }
 
-  axios.get(base_url + '/notif/send-clubApproval-email', { params })
+  axios.get(baseUrl + '/notif/send-clubApproval-email', { params })
     .then(function (resp) {
       console.log(resp)
     })
@@ -82,17 +87,17 @@ export const sendApproveClubTemplate = (club_email, club_name) => {
 export const sendPush = (uid, title, body, icon) => {
   let params = {
     uid: uid,
-	    notificationOptions: {
-	    	notification: {
-		      title: title,
-		      body: body,
-		      icon: icon || 'https://laracasts.com/images/series/circles/do-you-react.png',
-		      click_action: process.env.NODE_ENV === 'production' ? 'https://prod.cribblservices.com' : null
-		  	}
+    notificationOptions: {
+      notification: {
+        title: title,
+        body: body,
+        icon: icon || 'https://laracasts.com/images/series/circles/do-you-react.png',
+        click_action: process.env.NODE_ENV === 'production' ? 'https://prod.cribblservices.com' : null
+      }
     }
   }
 
-  axios.post(base_url + '/notif/send-push', params)
+  axios.post(baseUrl + '/notif/send-push', params)
     .then(function (resp) {
       console.log(resp)
     })
@@ -106,7 +111,7 @@ export const sendSMS = (to, message) => {
     phone: to,
     message: message
   }
-  axios.post(base_url + '/notif/send-sms', params)
+  axios.post(baseUrl + '/notif/send-sms', params)
     .then(function (resp) {
       console.log(resp)
     })
@@ -122,7 +127,7 @@ export const getNotificationRequestPermission = (uid) => {
       return firebaseMessaging.getToken()
     })
     .then(function (token) {
-  	sessionStorage.setItem('fcmToken', token)
+      sessionStorage.setItem('fcmToken', token) // eslint-disable-line
       updateToken(uid, token, true)
     })
     .catch(function (err) {
@@ -133,13 +138,14 @@ export const getNotificationRequestPermission = (uid) => {
 firebaseMessaging.onTokenRefresh(function () {
   firebaseMessaging.getToken()
     .then(function (token) {
-      updateToken(sessionStorage.getItem('uid'), token, true)
+      updateToken(sessionStorage.getItem('uid'), token, true) // eslint-disable-line
     })
     .catch(function (err) {
-      console.log('could not retrieve new token')
+      console.log('could not retrieve new token' + err)
     })
 })
 
+/*
 const getNotificationPermissionState = () => {
   navigator.permissions.query({ name: 'notifications' })
     .then(function (permissionStatus) {
@@ -150,46 +156,46 @@ const getNotificationPermissionState = () => {
       }
     })
 }
-
+*/
 firebaseMessaging.onMessage(function (payload) {
   store.dispatch(toggleActions.toggleToaster(payload.notification.title, true))
 })
 
 export const requestOTP = (uid, contact, callback) => {
   let params = {
-	  userID: uid,
-	  contact: contact
+    userID: uid,
+    contact: contact
   }
-  axios.get(base_url + '/send-otp', { params })
+  axios.get(baseUrl + '/send-otp', { params })
     .then(function (res) {
-	  if (res.data.code == 'failure') {
-	    throw res.data.message
-	  }
-	  console.log(res)
-	  callback(null)
+      if (res.data.code === 'failure') {
+        throw res.data.message
+      }
+      console.log(res)
+      callback(null)
     })
     .catch(function (err) {
-	  console.log(err)
-	  callback(err)
+      console.log(err)
+      callback(err)
     })
 }
 
 export const confirmOTP = (code, userDetails, callback) => {
   let params = {
-	  userID: userDetails.uid,
-	  code: code,
-	  userDetails: userDetails
+    userID: userDetails.uid,
+    code: code,
+    userDetails: userDetails
   }
-  axios.get(base_url + '/confirm-otp', { params })
+  axios.get(baseUrl + '/confirm-otp', { params })
     .then(function (res) {
-	  if (res.data.code == 'failure') {
-	    throw res.data.message
-	  }
-	  console.log(res)
-	  callback(null)
+      if (res.data.code === 'failure') {
+        throw res.data.message
+      }
+      console.log(res)
+      callback(null)
     })
     .catch(function (err) {
-	  console.log(err)
-	  callback(err)
+      console.log(err)
+      callback(err)
     })
 }
