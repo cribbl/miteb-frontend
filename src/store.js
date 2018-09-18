19019ab0dfe9cd96cf1b84ebd730897712ad740e
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-09-18 09:52:54 +0530

    style: store.js

--- src/store.js
+++ src/store.js
@@ -6,8 +6,8 @@ import rootReducer from './reducers'
 const middleware = [thunkMiddleware]
 
 // if(process.env.REACT_APP_NODE_ENV != 'production')
-// 	middleware.push(createLogger())
-if (window.location.host.indexOf('prod') == -1) { middleware.push(createLogger()) }
+// middleware.push(createLogger())
+if (window.location.host.indexOf('prod') === -1) { middleware.push(createLogger()) }
 
 export const store = createStore(
   rootReducer,
