a9a53a1b38ef6405df0d51c879ea3df9efe91844
arushi1912 <arushi_nigam@hotmail.com>
2018-12-27 00:45:48 +0530

    firebase Config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -12,3 +12,7 @@ var config = {
 firebase.initializeApp(config)
 
 export const firebaseDB = firebase.database()
+export const firebaseDB = firebase.database()
+export const firebaseAuth = firebase.auth()
+export const firebaseMessaging = firebase.messaging()
+export const storage = firebase.storage()
