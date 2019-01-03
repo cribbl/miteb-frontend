306949822275aa4587db6694532df1b13f429a3a
Rohit Sarkar <rohitsarkar5398@gmail.com>
2019-01-03 17:38:09 +0530

    fix: Block some rooms for SC.
    Block rooms in NLH first floor and 1 room(207) in AB5

--- src/components/ClassroomComponent/BookingComponent/Rooms.js
+++ src/components/ClassroomComponent/BookingComponent/Rooms.js
@@ -114,13 +114,13 @@ class RoomsContainer extends Component {
 
           <div style={{ width: this.props.isMobile ? '100%' : '35%', backgroundColor: '', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
             <h4>NLH</h4>
-            <div style={styles.buttonsRow}>
+            {/* <div style={styles.buttonsRow}>
               <MyRaisedButton id={3101} />
               <MyRaisedButton id={3102} />
               <MyRaisedButton id={3103} />
               <MyRaisedButton id={3104} />
               <MyRaisedButton id={3105} />
-            </div>
+            </div> */}
             <div style={styles.buttonsRow}>
               <MyRaisedButton id={3201} />
               <MyRaisedButton id={3202} />
@@ -154,7 +154,7 @@ class RoomsContainer extends Component {
               <MyRaisedButton id={5203} />
               <MyRaisedButton id={5205} />
               <MyRaisedButton id={5206} />
-              <MyRaisedButton id={5207} />
+              {/* <MyRaisedButton id={5207} /> */}
               <MyRaisedButton id={5208} />
             </div>
             <div style={styles.buttonsRow}>
