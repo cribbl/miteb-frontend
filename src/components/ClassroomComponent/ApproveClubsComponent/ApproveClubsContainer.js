084e7ea83bfb0a71a5aa9a485dc36e076a510eda
Rohit Sarkar <rohitsarkar5398@gmail.com>
2019-09-12 10:48:10 +0530

    fix: filter undefined values from arr in approve clubs

--- src/components/ClassroomComponent/ApproveClubsComponent/ApproveClubsContainer.js
+++ src/components/ClassroomComponent/ApproveClubsComponent/ApproveClubsContainer.js
@@ -53,6 +53,7 @@ class ApproveClubsContainer extends Component {
     var tempArr = scope.state.tempArr
     var approvedArr = scope.state.approvedArr
     var unapprovedArr = scope.state.unapprovedArr
+    // TODO: Move to Services
     firebaseDB.ref().child('users').on('value',
       function (snapshot) {
         scope.setState({ fetching: false })
@@ -192,33 +193,35 @@ class ApproveClubsContainer extends Component {
                   }
 
                   {
-                    Object.keys(this.state.tempArr).length > 0 ? (Object.values(this.state.tempArr).map(function (club, index) {
-                      return (
-                        <TableRow key={index}>
-                          <TableRowColumn style={{ width: '10%' }}><StatusIcon style={{ color: club.isApproved ? '#558B2F' : '#b71c1c' }} data-tip='dfdsf' /></TableRowColumn>
-                          <TableRowColumn style={{ width: '30%' }}>{club.name}</TableRowColumn>
-                          <TableRowColumn hidden={this.props.isMobile} style={{ width: '20%' }}>{club.category}</TableRowColumn>
-                          <TableRowColumn hidden={this.props.isMobile} style={{ width: '30%' }}>{club.fa.name}</TableRowColumn>
-                          <TableRowColumn style={{ width: this.props.isMobile ? '14%' : '10%', textOverflow: 'clip' }}>
-                            {<IconMenu
-                              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-                              anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
-                              targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-                              useLayerForClickAway
-                            >
-                              <MenuItem primaryText='View' onClick={() => this.showDialog(club)} />
-                              <MenuItem primaryText={club.isApproved ? 'Mark as Unapproved' : 'Mark as Approved'} onClick={() => this.approveClub(club, !club.isApproved)} />
-                            </IconMenu>}
-                          </TableRowColumn>
-                        </TableRow>
+                    Object.keys(this.state.tempArr).length > 0 ? (Object.values(this.state.tempArr)
+                      .filter((elem) => elem !== undefined)
+                      .map(function (club, index) {
+                        return (
+                          <TableRow key={index}>
+                            <TableRowColumn style={{ width: '10%' }}><StatusIcon style={{ color: club.isApproved ? '#558B2F' : '#b71c1c' }} data-tip='dfdsf' /></TableRowColumn>
+                            <TableRowColumn style={{ width: '30%' }}>{club.name}</TableRowColumn>
+                            <TableRowColumn hidden={this.props.isMobile} style={{ width: '20%' }}>{club.category}</TableRowColumn>
+                            <TableRowColumn hidden={this.props.isMobile} style={{ width: '30%' }}>{club.fa.name}</TableRowColumn>
+                            <TableRowColumn style={{ width: this.props.isMobile ? '14%' : '10%', textOverflow: 'clip' }}>
+                              {<IconMenu
+                                iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+                                anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                useLayerForClickAway
+                              >
+                                <MenuItem primaryText='View' onClick={() => this.showDialog(club)} />
+                                <MenuItem primaryText={club.isApproved ? 'Mark as Unapproved' : 'Mark as Approved'} onClick={() => this.approveClub(club, !club.isApproved)} />
+                              </IconMenu>}
+                            </TableRowColumn>
+                          </TableRow>
+                        )
+                      }, this))
+                      : (
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: this.props.isMobile ? '15%' : '2%', textAlign: 'center', minHeight: 250 }} hidden={this.state.fetching}>
+                          <img src={require(this.state.searchContent.length > 0 ? '../../../assets/empty-state.gif' : '../../../assets/empty-state.gif')} style={{ width: this.props.isMobile ? '70%' : '30%', marginBottom: 10 }} />
+                          <p>{this.state.searchContent.length > 0 ? 'No clubs for this search' : 'No clubs here'}</p>
+                        </div>
                       )
-                    }, this)) : (
-
-                      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: this.props.isMobile ? '15%' : '2%', textAlign: 'center', minHeight: 250 }} hidden={this.state.fetching}>
-                        <img src={require(this.state.searchContent.length > 0 ? '../../../assets/empty-state.gif' : '../../../assets/empty-state.gif')} style={{ width: this.props.isMobile ? '70%' : '30%', marginBottom: 10 }} />
-                        <p>{this.state.searchContent.length > 0 ? 'No clubs for this search' : 'No clubs here'}</p>
-                      </div>
-                    )
                   }
                 </TableBody>
               </Table>
